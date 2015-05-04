bdd28b93bd9e0a7784983ead1f800e2dd1019608
Artur Rozart Różalski <rozart@gmail.com>
2015-05-05 00:59:46 +0200

    Basic calculator actions implemented

--- src/pl/rozart/groovycalculator/CalculatorGUI.groovy
+++ src/pl/rozart/groovycalculator/CalculatorGUI.groovy
@@ -12,12 +12,16 @@ import java.awt.GraphicsEnvironment
 
 public class CalculatorGUI {
 
+    private Stack<Object> calcStack = new Stack<>();
+
     private SwingBuilder swingBuilder = new SwingBuilder()
     private Font buttonFont = new Font("Impact", Font.BOLD, 30)
 
     private Font calculatorFont
     private JFrame frame
     private JTextField display
+
+    private GroovyShell groovyShell = new GroovyShell()
 
     public static void main(String[] args){
         CalculatorGUI calculator = new CalculatorGUI();
@@ -49,25 +53,25 @@ public class CalculatorGUI {
             panel(constraints: BorderLayout.CENTER, background: java.awt.Color.LIGHT_GRAY) {
                 gridLayout(cols: 5, rows: 4, vgap: 10, hgap: 10)
 
-                button(text: "7", horizontalTextPosition: JButton.CENTER, font:  buttonFont, actionPerformed: {calculatorAction("7")})
-                button(text: "8", horizontalTextPosition: JButton.CENTER, font:  buttonFont, actionPerformed: {calculatorAction("8")})
-                button(text: "9", horizontalTextPosition: JButton.CENTER, font:  buttonFont, actionPerformed: {calculatorAction("9")})
+                button(text: "7", horizontalTextPosition: JButton.CENTER, font:  buttonFont, actionPerformed: {calculatorAction(7)})
+                button(text: "8", horizontalTextPosition: JButton.CENTER, font:  buttonFont, actionPerformed: {calculatorAction(8)})
+                button(text: "9", horizontalTextPosition: JButton.CENTER, font:  buttonFont, actionPerformed: {calculatorAction(9)})
                 button(text: "/", horizontalTextPosition: JButton.CENTER, font:  buttonFont, actionPerformed: {calculatorAction("/")})
                 button(text: "*", horizontalTextPosition: JButton.CENTER, font:  buttonFont, actionPerformed: {calculatorAction("*")})
 
-                button(text: "4", horizontalTextPosition: JButton.CENTER, font:  buttonFont, actionPerformed: {calculatorAction("4")})
-                button(text: "5", horizontalTextPosition: JButton.CENTER, font:  buttonFont, actionPerformed: {calculatorAction("5")})
-                button(text: "6", horizontalTextPosition: JButton.CENTER, font:  buttonFont, actionPerformed: {calculatorAction("6")})
+                button(text: "4", horizontalTextPosition: JButton.CENTER, font:  buttonFont, actionPerformed: {calculatorAction(4)})
+                button(text: "5", horizontalTextPosition: JButton.CENTER, font:  buttonFont, actionPerformed: {calculatorAction(5)})
+                button(text: "6", horizontalTextPosition: JButton.CENTER, font:  buttonFont, actionPerformed: {calculatorAction(6)})
                 button(text: "-", horizontalTextPosition: JButton.CENTER, font:  buttonFont, actionPerformed: {calculatorAction("-")})
                 button(text: "+", horizontalTextPosition: JButton.CENTER, font:  buttonFont, actionPerformed: {calculatorAction("+")})
 
-                button(text: "1", horizontalTextPosition: JButton.CENTER, font:  buttonFont, actionPerformed: {calculatorAction("1")})
-                button(text: "2", horizontalTextPosition: JButton.CENTER, font:  buttonFont, actionPerformed: {calculatorAction("2")})
-                button(text: "3", horizontalTextPosition: JButton.CENTER, font:  buttonFont, actionPerformed: {calculatorAction("3")})
+                button(text: "1", horizontalTextPosition: JButton.CENTER, font:  buttonFont, actionPerformed: {calculatorAction(1)})
+                button(text: "2", horizontalTextPosition: JButton.CENTER, font:  buttonFont, actionPerformed: {calculatorAction(2)})
+                button(text: "3", horizontalTextPosition: JButton.CENTER, font:  buttonFont, actionPerformed: {calculatorAction(3)})
                 button(text: "C", horizontalTextPosition: JButton.CENTER, font:  buttonFont, actionPerformed: {calculatorAction("C")})
                 button(text: "CE", horizontalTextPosition: JButton.CENTER, font:  buttonFont, actionPerformed: {calculatorAction("CE")})
 
-                button(text: "0", horizontalTextPosition: JButton.CENTER, font:  buttonFont, actionPerformed: {calculatorAction("0")})
+                button(text: "0", horizontalTextPosition: JButton.CENTER, font:  buttonFont, actionPerformed: {calculatorAction(0)})
                 button(text: "=", horizontalTextPosition: JButton.CENTER, font:  buttonFont, actionPerformed: {calculatorAction("=")})
             }
 
@@ -75,11 +79,90 @@ public class CalculatorGUI {
         }
     }
 
-    private void calculatorAction(String actionValue){
-        this.display.setText(actionValue);
+    private void calculatorAction(Object actionValue){
+            if(actionValue instanceof Integer){
+                if(calcStack.isEmpty()){
+                    calcStack.push(actionValue)
+                    display.setText(actionValue.toString())
+                }else{
+                    if(calcStack.peek() instanceof Integer){
+                        Integer number = calcStack.pop()
+                        number = Integer.parseInt(number.toString() + actionValue)
+                        calcStack.push(number)
+                        display.setText(number.toString())
+                    }else{
+                        calcStack.push(actionValue)
+                        display.setText(actionValue.toString())
+                    }
+                }
+            }else{
+                String action = (String) actionValue
+                addOperation(action)
+            }
+
+
+
     }
 
+    private void addOperation(String operation){
+        switch(operation){
+            case "C":
+                calcStack = new Stack<>()
+                display.setText("0")
+                break;
+            case "CE":
+                if(!calcStack.empty()){
+                    Object lastStackObject = calcStack.pop()
+                    if(lastStackObject instanceof String){
+                        display.setText(calcStack.peek())
+                    }else if(!calcStack.empty()){
+                        lastStackObject = calcStack.pop()
+                        display.setText(calcStack.peek())
+                        calcStack.push(lastStackObject)
+                    }else{
+                        calcStack = new Stack<>()
+                        display.setText("0")
+                    }
+                }
+                break;
+            case "=":
+                calculateResult()
+                break;
+            default:
+                if(!calcStack.empty() && calcStack.peek() instanceof  Integer){
+                    calcStack.push(operation)
+                }
+        }
+    }
 
+    private Integer calculateResult(){
+        if(isStackReadyForCalculation()){
+
+            String rowToCalculate = ""
+            for(Object stackObject : calcStack) {
+                rowToCalculate += stackObject
+            }
+
+            try {
+                Integer result = groovyShell.evaluate(rowToCalculate)
+                calcStack = new Stack<>()
+                calcStack.push(result)
+                display.setText(result.toString())
+            }catch(ArithmeticException e){
+                println "Attempt of division by zero!"
+                calcStack = new Stack<>()
+                display.setText("0")
+            }
+
+        }
+    }
+
+    private boolean isStackReadyForCalculation(){
+        if(calcStack.size() > 1 && calcStack.size() % 2 == 1){
+            return true;
+        }
+        return false;
+    }
 
 
 
