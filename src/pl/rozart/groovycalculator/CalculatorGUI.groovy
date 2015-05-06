c3726b7e2a9d219b33cfa9e36b6b549afa609f2a
Artur Rozart Różalski <rozart@gmail.com>
2015-05-07 00:49:22 +0200

    General refactor

--- src/pl/rozart/groovycalculator/CalculatorGUI.groovy
+++ src/pl/rozart/groovycalculator/CalculatorGUI.groovy
@@ -13,7 +13,8 @@ import java.text.NumberFormat
 
 public class CalculatorGUI {
 
-    private Stack<Object> calcStack = new Stack<>();
+    private Stack<Object> calculatorStack = new Stack<>();
+    private String displayString = "0";
 
     private SwingBuilder swingBuilder = new SwingBuilder()
     private Font buttonFont = new Font("Impact", Font.BOLD, 30)
@@ -24,7 +25,6 @@ public class CalculatorGUI {
 
     private GroovyShell groovyShell = new GroovyShell()
 
-    private String lastDoubleString = "";
 
     public static void main(String[] args){
         CalculatorGUI calculator = new CalculatorGUI();
@@ -56,156 +56,99 @@ public class CalculatorGUI {
             panel(constraints: BorderLayout.CENTER, background: java.awt.Color.LIGHT_GRAY) {
                 gridLayout(cols: 5, rows: 4, vgap: 10, hgap: 10)
 
-                button(text: "7", horizontalTextPosition: JButton.CENTER, font:  buttonFont, actionPerformed: {calculatorAction(7L)})
-                button(text: "8", horizontalTextPosition: JButton.CENTER, font:  buttonFont, actionPerformed: {calculatorAction(8L)})
-                button(text: "9", horizontalTextPosition: JButton.CENTER, font:  buttonFont, actionPerformed: {calculatorAction(9L)})
-                button(text: "/", horizontalTextPosition: JButton.CENTER, font:  buttonFont, actionPerformed: {calculatorAction("/")})
-                button(text: "*", horizontalTextPosition: JButton.CENTER, font:  buttonFont, actionPerformed: {calculatorAction("*")})
+                button(text: "7", horizontalTextPosition: JButton.CENTER, font:  buttonFont, actionPerformed: {addDigitToDisplay("9")})
+                button(text: "8", horizontalTextPosition: JButton.CENTER, font:  buttonFont, actionPerformed: {addDigitToDisplay("8")})
+                button(text: "9", horizontalTextPosition: JButton.CENTER, font:  buttonFont, actionPerformed: {addDigitToDisplay("7")})
+                button(text: "/", horizontalTextPosition: JButton.CENTER, font:  buttonFont, actionPerformed: {addOperationToStack("/")})
+                button(text: "*", horizontalTextPosition: JButton.CENTER, font:  buttonFont, actionPerformed: {addOperationToStack("*")})
 
-                button(text: "4", horizontalTextPosition: JButton.CENTER, font:  buttonFont, actionPerformed: {calculatorAction(4L)})
-                button(text: "5", horizontalTextPosition: JButton.CENTER, font:  buttonFont, actionPerformed: {calculatorAction(5L)})
-                button(text: "6", horizontalTextPosition: JButton.CENTER, font:  buttonFont, actionPerformed: {calculatorAction(6L)})
-                button(text: "-", horizontalTextPosition: JButton.CENTER, font:  buttonFont, actionPerformed: {calculatorAction("-")})
-                button(text: "+", horizontalTextPosition: JButton.CENTER, font:  buttonFont, actionPerformed: {calculatorAction("+")})
+                button(text: "4", horizontalTextPosition: JButton.CENTER, font:  buttonFont, actionPerformed: {addDigitToDisplay("4")})
+                button(text: "5", horizontalTextPosition: JButton.CENTER, font:  buttonFont, actionPerformed: {addDigitToDisplay("5")})
+                button(text: "6", horizontalTextPosition: JButton.CENTER, font:  buttonFont, actionPerformed: {addDigitToDisplay("6")})
+                button(text: "-", horizontalTextPosition: JButton.CENTER, font:  buttonFont, actionPerformed: {addOperationToStack("-")})
+                button(text: "+", horizontalTextPosition: JButton.CENTER, font:  buttonFont, actionPerformed: {addOperationToStack("+")})
 
-                button(text: "1", horizontalTextPosition: JButton.CENTER, font:  buttonFont, actionPerformed: {calculatorAction(1L)})
-                button(text: "2", horizontalTextPosition: JButton.CENTER, font:  buttonFont, actionPerformed: {calculatorAction(2L)})
-                button(text: "3", horizontalTextPosition: JButton.CENTER, font:  buttonFont, actionPerformed: {calculatorAction(3L)})
-                button(text: "C", horizontalTextPosition: JButton.CENTER, font:  buttonFont, actionPerformed: {calculatorAction("C")})
-                button(text: "CE", horizontalTextPosition: JButton.CENTER, font:  buttonFont, actionPerformed: {calculatorAction("CE")})
+                button(text: "1", horizontalTextPosition: JButton.CENTER, font:  buttonFont, actionPerformed: {addDigitToDisplay("1")})
+                button(text: "2", horizontalTextPosition: JButton.CENTER, font:  buttonFont, actionPerformed: {addDigitToDisplay("2")})
+                button(text: "3", horizontalTextPosition: JButton.CENTER, font:  buttonFont, actionPerformed: {addDigitToDisplay("3")})
+                button(text: "C", horizontalTextPosition: JButton.CENTER, font:  buttonFont, actionPerformed: {clearAll()})
+                button(text: "CE", horizontalTextPosition: JButton.CENTER, font:  buttonFont, actionPerformed: {clearDisplay()})
 
-                button(text: ".", horizontalTextPosition: JButton.CENTER, font: buttonFont, actionPerformed: {calculatorAction(".")})
-                button(text: "0", horizontalTextPosition: JButton.CENTER, font:  buttonFont, actionPerformed: {calculatorAction(0L)})
-                button(text: "=", horizontalTextPosition: JButton.CENTER, font:  buttonFont, actionPerformed: {calculatorAction("=")})
+                button(text: ".", horizontalTextPosition: JButton.CENTER, font: buttonFont, actionPerformed: {addDecimalPoint(".")})
+                button(text: "0", horizontalTextPosition: JButton.CENTER, font:  buttonFont, actionPerformed: {addDigitToDisplay("0")})
+                button(text: "=", horizontalTextPosition: JButton.CENTER, font:  buttonFont, actionPerformed: {calculate()})
             }
 
 
         }
     }
 
-    private void calculatorAction(Object actionValue){
-        if(actionValue instanceof Number) {
-            if (calcStack.isEmpty()) {
-                calcStack.push(actionValue)
-                display.setText(actionValue.toString())
+    private void addDigitToDisplay(String digit){
+        if(calculatorStack.size() == 2 ){
+            if (displayString.equals("0")) {
+                displayString = digit
             } else {
-                if (calcStack.peek() instanceof Long) {
-                    actionForLong(actionValue)
-                }else if(calcStack.peek() instanceof Double){
-                    actionForDouble(actionValue)
-                }else {
-                    calcStack.push(actionValue)
-                    display.setText(actionValue.toString())
-                }
+                displayString += digit
             }
+        }else {
+            if (displayString.equals("0")) {
+                displayString = digit
+            } else {
+                displayString += digit
+            }
+        }
+        display.setText(displayString)
+    }
+
+    private void addOperationToStack(String operation){
+        if(calculatorStack == 3){
+            calculate()
+            calculatorStack.push(operation)
+            displayString = ""
+            println "Calculated and pushed operation: " + operation
+        }else if(calculatorStack.empty()){
+            BigDecimal number = new BigDecimal(displayString)
+            calculatorStack.push(number)
+            calculatorStack.push(operation)
+            displayString = ""
+            println "Pushed operation: " + operation
+        }
+    }
+
+    private void calculate(){
+        if(calculatorStack.size() == 2){
+            BigDecimal number = new BigDecimal(displayString)
+            calculatorStack.push(number)
+
+            String calculateString = "";
+            for(Object calculatorElement : calculatorStack){
+                calculateString += calculatorElement.toString()
+            }
+
+            BigDecimal result = 0;
+            result = groovyShell.evaluate(calculateString)
+            displayString = result.toString()
+            display.setText(displayString)
+
+            calculatorStack = new Stack<>()
+            calculatorStack.push(result)
+            println "Result is: " + displayString
+        }
+    }
+
+    private void clearAll(){
+        calculatorStack = new Stack<>()
+        displayString = "0";
+        display.setText(displayString)
+    }
+
+    private void clearDisplay(){
+        if(calculatorStack.size() == 2 && !displayString.equals("0")){
+            displayString = "0"
+            display.setText(displayString)
         }else{
-            String action = (String) actionValue
-            addOperation(action)
+            clearAll()
         }
     }
-
-    private void actionForLong(Number selectedNumber){
-        Long number = calcStack.pop()
-        try {
-            number = Long.parseLong(number.toString() + selectedNumber)
-        }catch(NumberFormatException e){
-            println "Number is too long"
-        }
-        calcStack.push(number)
-        display.setText(number.toString())
-    }
-
-    private void actionForDouble(Number selectedNumber){
-        Double numberFromStack =  calcStack.pop()
-        String numberFromStackAsString = numberFromStack.toString()
-        if(numberFromStackAsString.endsWith(".0")){
-            numberFromStackAsString = numberFromStackAsString.substring(0, numberFromStackAsString.length() - 1)
-        }
-        if(!lastDoubleString.isEmpty()){
-            numberFromStackAsString = lastDoubleString
-        }
-        numberFromStackAsString += selectedNumber
-        if(selectedNumber == 0){
-            lastDoubleString = numberFromStackAsString
-        }else{
-            lastDoubleString = ""
-        }
-        numberFromStack = Double.parseDouble(numberFromStackAsString)
-        calcStack.push(numberFromStack)
-        display.setText(numberFromStackAsString)
-    }
-
-    private void addOperation(String operation){
-        switch(operation){
-            case "C":
-                calcStack = new Stack<>()
-                display.setText("0")
-                break;
-            case "CE":
-                if(!calcStack.empty()){
-                    Object lastStackObject = calcStack.pop()
-                    if(lastStackObject instanceof String){
-                        display.setText(calcStack.peek())
-                    }else if(!calcStack.empty()){
-                        lastStackObject = calcStack.pop()
-                        display.setText(calcStack.peek())
-                        calcStack.push(lastStackObject)
-                    }else{
-                        calcStack = new Stack<>()
-                        display.setText("0")
-                    }
-                }
-                break;
-            case "=":
-                calculateResult()
-                break;
-            case ".":
-                if(!calcStack.empty()){
-                    Object lastStackObject = calcStack.pop()
-                    if(lastStackObject instanceof Long){
-                        Double newStackObject = lastStackObject.doubleValue()
-                        calcStack.push(newStackObject)
-                        display.setText(newStackObject.toString())
-                    }
-                }
-                break;
-            default:
-                if(!calcStack.empty() && calcStack.peek() instanceof  Number){
-                    calcStack.push(operation)
-                }
-        }
-    }
-
-    private Long calculateResult(){
-        if(isStackReadyForCalculation()){
-
-            String rowToCalculate = ""
-            for(Object stackObject : calcStack) {
-                rowToCalculate += stackObject
-            }
-
-            try {
-                Number result = groovyShell.evaluate(rowToCalculate)
-                calcStack = new Stack<>()
-                calcStack.push(result)
-                display.setText(result.toString())
-            }catch(ArithmeticException e){
-                println "Attempt of division by zero!"
-                calcStack = new Stack<>()
-                display.setText("0")
-            }
-
-        }
-    }
-
-    private boolean isStackReadyForCalculation(){
-        if(calcStack.size() > 1 && calcStack.size() % 2 == 1){
-            return true;
-        }
-        return false;
-    }
-
-
-
 
 }
